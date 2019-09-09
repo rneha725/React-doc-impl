581622352ccb05e66c3513edad44fec29ec3a64f
Neha <rneha725@gmail.com>
2019-09-09 16:19:15 +0530

    Add more concept code

--- src/index.js
+++ src/index.js
@@ -70,23 +70,17 @@ ReactDOM.render(<Seconds />, document.getElementById("seconds"));
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { status: 0, text: { "1": "ON", "0": "OFF" } };
+    this.state = { status: true, text: { true: "ON", false: "OFF" } };
   }
 
-  toggle() {
-    let st = this.state.status;
-    if (st === 0) {
-      st = 1;
-    } else if (st === 1) {
-      st = 0;
-    }
-    this.setState({ status: st });
+  toggle(e) {
+    this.setState(state => ({ status: !state.status }));
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.toggle}>
+        <button onClick={this.toggle.bind(this)}>
           {this.state.text[this.state.status]}
         </button>
         <hr />
@@ -96,3 +90,66 @@ class Toggle extends React.Component {
 }
 
 ReactDOM.render(<Toggle />, document.getElementById("toggle"));
+
+//------------------------------------------------------------------------------
+// JS experiment
+let ar = [1, 2, 3];
+ar = ar.map(i => 2 * i);
+
+console.log(ar);
+// in react element, each element in array is rendered
+
+class ListItem extends React.Component {
+  render() {
+    return (
+      <ul>
+        {ar.map(i => {
+          return <li key={i}>{2 * i}</li>;
+        })}
+        <hr />
+      </ul>
+    );
+  }
+}
+
+ReactDOM.render(<ListItem />, document.getElementById("list"));
+
+//------------------------------------------------------------------------------
+// create a form, controlled by state
+
+class FormControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    // 1. in case of state not used, below two lines are used
+    // let name = document.getElementById("name").value;
+    // alert("Hello " + name + "!");
+    // 2. when using states
+    alert("Hello " + this.state.name + "!");
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value.toUpperCase() });
+  }
+  render() {
+    return (
+      <form>
+        <input
+          type="input"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+        <button type="submit" onClick={this.handleSubmit}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<FormControl />, document.getElementById("form"));
